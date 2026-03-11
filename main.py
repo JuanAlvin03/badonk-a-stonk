d6fb75b753ae3a44795a98ee82507ab94121396f
JuanAlvin03 <juanalvinss03@gmail.com>
2026-03-11 13:21:40 +0700

    added financial data
    in form of dataframe for now

--- main.py
+++ main.py
@@ -5,13 +5,21 @@ import plotly.graph_objects as go
 from datetime import datetime
 from plotly.subplots import make_subplots
 
+#==================================================================
+# Page Configurations
+#==================================================================
+
 st.set_page_config(layout="wide")
+
+#==================================================================
+# Logic
+#==================================================================
 
 ticker = 'AAPL'
 #stockdf = yf.download(ticker, start='2023-01-01', end='2024-01-01')
 stock = yf.Ticker(ticker)
 #stockdf = stock.history(period='1y')
-stockdf = stock.history(period='2d', interval='15m')
+stockdf = stock.history(period='1d', interval='15m')
 
 # Intervals: Valid options include 1m, 2m, 5m, 15m, 30m, 60m, 90m, and 1h.
 # data = yf.download(tickers="AAPL", period="5d", interval="1m")
@@ -40,16 +48,45 @@ fig.add_trace(go.Bar(x=stockdf.index, y=stockdf['Volume'], name='Volume'), row=2
 
 fig.update_layout(xaxis_rangeslider_visible=False)
 
-#st.markdown('### <Ticker>: <Stock/company Name>')
+#==================================================================
+# Main Column
+#==================================================================
+col1, col2 = st.columns([6, 2])
 
-col1, col2 = st.columns([3, 1])
+with col1:
+    st.markdown(f'### ${ticker}: {stock.info["longName"]}')
+    st.markdown(f'### ${stockdf["Close"].iloc[-1]:.2f} ({(stockdf["Close"].iloc[-1] - stockdf["Open"].iloc[0]) / stockdf["Open"].iloc[0] * 100:.2f}%)')
+    st.plotly_chart(fig, width='stretch')
+    st.write("Candlestick chart for stock data, put average, volume, etc.")
+    st.write("Time control for chart")
 
-col1.markdown(f'### ${ticker}: {stock.info["longName"]}')
-col1.markdown(f'### ${stockdf["Close"].iloc[-1]:.2f} ({(stockdf["Close"].iloc[-1] - stockdf["Open"].iloc[0]) / stockdf["Open"].iloc[0] * 100:.2f}%)')
-col1.plotly_chart(fig, width='stretch')
-col1.write("Candlestick chart for stock data, put average, volume, etc.")
-col1.write("Time control for chart")
+with col2:
+    st.write("Date/Time: ", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    findata = pd.DataFrame({'EPS(TTM)': [stock.info['trailingEps']], 
+                            'PE Ratio(TTM)': [stock.info['trailingPE']],
+                            'Market Cap': [stock.info['marketCap']],
+                            'Dividend Yield': [stock.info['dividendYield']],
+                            'Volume': [stock.info['volume']],
+                            'Average Volume': [stock.info['averageVolume']],
+                            '52 Week Low': [stock.info['fiftyTwoWeekLow']],
+                            '52 Week High': [stock.info['fiftyTwoWeekHigh']]
+                            })
+    
+    findata = findata.transpose()
+    st.dataframe(findata)
 
-col2.markdown('#### Current Date/Time')
-col2.subheader("A narrow column with the data")
-col2.write("Financial info for ticker/stock")
+    #subcol1, subcol2 = st.columns(2)
+
+    #with subcol1:
+    #    st.markdown("**EPS(TTM)**")
+    #    st.markdown("{:.2f}".format(stock.info['trailingEps']))
+    #with subcol2:
+    #    st.markdown("**PE Ratio(TTM)**  " \
+    #    "{:.2f}".format(stock.info['trailingPE']))
+
+#==================================================================
+# End of Main Column
+#==================================================================
+
+with st.expander("About the Company"):
+    st.write(stock.info['longBusinessSummary'])
